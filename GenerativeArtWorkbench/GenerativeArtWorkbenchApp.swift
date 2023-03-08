ca100ef685478bb430c1c4ee05e7e8949369459b
imk2o <imk2o@icloud.com>
2023-03-08 09:04:06 +0900

    Initial Commit

---
+++ GenerativeArtWorkbench/GenerativeArtWorkbenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeArtWorkbenchApp.swift
+//  GenerativeArtWorkbench
+//
+//  Created by k2o on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeArtWorkbenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
